80299b48d65983f409658bd7bb1437f1db3257c1
DouglasPatton <douglaspatton@gmail.com>
2019-06-02 07:14:37 -0400

    xml read write working

--- USGShydro.py
+++ USGShydro.py
@@ -1,17 +1,17 @@
 import gzip
 from urllib.request import urlopen, Request
-from urllib import request
-import os
 
+import os
+import xml.etree.ElementTree as ET
 
 class Hydrosite():
     '''makes parent class hydrosite, child will be hydrositedata, and grandchild is hydrositedatamodel. '''
     def __init__(self, site):
         self.site=site
         self.sitecode=site[-8:]
-        directory='usgs-huc8-'+self.sitecode
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+        self.sitedirectory='data/usgs-huc8-'+self.sitecode
+        if not os.path.exists(self.sitedirectory):
+            os.makedirs(self.sitedirectory)
 
     
 class Hydrositedata(Hydrosite): 
@@ -27,11 +27,22 @@ class Hydrositedata(Hydrosite):
         baseurl='https://waterservices.usgs.gov/nwis/iv/?'
         theformat='format=waterml,2.0'
         parameter='parameterCd='+','.join(self.paramlist)
-        requesturl = baseurl+theformat+'&'+self.site+'&'+self.start+'&'+self.end+'&'+parameter+'&'+'siteStatus=all'
-        print(requesturl)
-        #response=urlopen(Request(requesturl,headers={"Accept-Encoding": "gzip"})) 
-        #return gzip.open(response, 'rb').read()
-
+        urlsuffix=theformat+'&'+self.site+'&'+self.start+'&'+self.end+'&'+parameter+'&'+'siteStatus=all'
+        self.requesturl=baseurl+urlsuffix
+        self.datapath=os.path.join(self.sitedirectory,urlsuffix+'.xml')
+        #print(requesturl)
+        if os.path.exists(self.datapath):
+            self.data=ET.parse(self.datapath) #add try clause?
+            self.root=self.data.getroot() 
+        else:    
+            response=urlopen(Request(self.requesturl,headers={"Accept-Encoding": "gzip"})) 
+            self.data=gzip.open(response, 'rb').read()
+            self.root=ET.fromstring(self.data)
+            #working on save problems:
+            savefile=open(self.datapath,'w')
+            savefile.write(ET.tostring(self.root).decode("utf-8"))
+            savefile.close()
+            #self.root.xml.write(self.datapath)
 
 class Hydrositedatamodel(Hydrositedata):
     '''makes grandchild of hydrosite,child of hydrositedata'''
