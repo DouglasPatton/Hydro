a1d989314887b6641fa08a7b0a5ea412c8d508d8
DouglasPatton <douglaspatton@gmail.com>
2019-06-13 16:49:12 -0400

    working on RR lag model

--- rainfallrunoff.py
+++ rainfallrunoff.py
@@ -4,23 +4,22 @@ class RRtimeseries():
     '''take in column of dates, rainfall, and runoff and builds distributed lag model'''
     def __init__(self,data,modelfeatures):
         self.data=data
+        self.n=np.shape(data)[0]
         self.modelfeatures=modelfeatures
         if modelfeatures['RRmodeltype']=='distributed_lag':
             if modelfeatures['local']=='no':
-                maxlag=modelfeatures['maxlag']
+                self.maxlag=modelfeatures['maxlag']
                 self.distlagmodel()
         else: print('other')
         
     def distlagmodel(self):
-        n,k=data.shape()
-        
-        self.lagprecip=self.lagmaker(data[:,1],maxlags)
+        self.lagprecip=self.lagmaker(self.data[:,1],self.maxlag)
         
         
         
-    def lagmaker(self,dataseries,maxlag):  
-        lagprecip=np.ones(n+maxlag,maxlag+1) #+1 b/c 0...maxlag
-        for i in range(maxlag+1):
-            lagprecip[[i:n],i]=data[:,1]
-        self.lagprecip=lagprecip[maxlag:(n+maxlag),:]
+    def lagmaker(self,precip_series,maxlag,startlag=0):  
+        lagprecip=np.ones(self.n+maxlag-startlag,maxlag-startlag+1) #+1 b/c 0...maxlag
+        for i in range(maxlag-startlag+1):
+            lagprecip[(i+startlag):self.n,i]=precip_series
+        self.lagprecip=lagprecip[maxlag:(self.n+maxlag),:]
         
