28b0df5f0d7bdffead415ee74a6a50419ade3676
DouglasPatton <douglaspatton@gmail.com>
2019-06-14 07:21:27 -0400

    fully lagged X data structure appears to work

--- rainfallrunoff.py
+++ rainfallrunoff.py
@@ -1,5 +1,7 @@
 
 import numpy as np
+from scipy.optimize import minimize
+
 class RRtimeseries():
     '''take in column of dates, rainfall, and runoff and builds distributed lag model'''
     def __init__(self,data,modelfeatures):
@@ -9,17 +11,30 @@ class RRtimeseries():
         if modelfeatures['RRmodeltype']=='distributed_lag':
             if modelfeatures['local']=='no':
                 self.maxlag=modelfeatures['maxlag']
+                self.startlag=modelfeatures['startlag']
                 self.distlagmodel()
         else: print('other')
         
     def distlagmodel(self):
-        self.lagprecip=self.lagmaker(self.data[:,1],self.maxlag)
+        self.lagprecip=self.lagmaker(self.data[:,1],self.maxlag,self.startlag)
+        if self.modelfeatures['AR1']=='yes':
+            self.lagn=np.shape(self.lagprecip)[0]
+            runlag1=self.data[(self.maxlag-1):(self.n-1),0][:,None]
+            print('***',runlag1.shape,self.lagprecip.shape)
+            X=np.concatenate((runlag1,self.lagprecip),axis=1)
+            print(np.shape(X))
+            #return minimize(lagmodelMSE,wt_try0,args=(runY,rainX),method='Nelder-Mead')
         
+    def lagmodelMSE(Betas,Y,X):
+        pass
+    
+    
         
-        
-    def lagmaker(self,precip_series,maxlag,startlag=0):  
-        lagprecip=np.ones(self.n+maxlag-startlag,maxlag-startlag+1) #+1 b/c 0...maxlag
+    def lagmaker(self,tseries,maxlag,startlag):  
+        lagtseries=np.empty([self.n+maxlag-startlag,maxlag-startlag+1]) #+1 b/c 0...maxlag
         for i in range(maxlag-startlag+1):
-            lagprecip[(i+startlag):self.n,i]=precip_series
-        self.lagprecip=lagprecip[maxlag:(self.n+maxlag),:]
+            lagtseries[(i+startlag):self.n+i+startlag,i]=tseries
+        return lagtseries[maxlag:self.n,:]
+        
+        
         
